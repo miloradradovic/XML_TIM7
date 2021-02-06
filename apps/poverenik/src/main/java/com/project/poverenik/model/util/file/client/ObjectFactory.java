9b8de15540fbacafa69f5d467351230af31c5d7f
romana98 <romana_1998@hotmail.com>
2021-02-06 14:12:58 +0100

    soap rdf json izvestaj

---
+++ apps/poverenik/src/main/java/com/project/poverenik/model/util/file/client/ObjectFactory.java
@@ -0,0 +1,27 @@
+package com.project.poverenik.model.util.file.client;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+@XmlRegistry
+public class ObjectFactory {
+
+    public ObjectFactory() {
+
+    }
+
+    public sendRdfFile createSendRdfFile() {
+        return new sendRdfFile();
+    }
+
+    public sendRdfFileResponse createSendRdfFileResponse() {
+        return new sendRdfFileResponse();
+    }
+
+    public sendJsonFile createSendJsonFile() {
+        return new sendJsonFile();
+    }
+
+    public sendJsonFileResponse createSendJsonFileResponse() {
+        return new sendJsonFileResponse();
+    }
+}
